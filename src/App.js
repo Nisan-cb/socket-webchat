7bfa78a54f54e0826d1b5636993c8c0c6811d976
Nis C <jobmail858@gmail.com>
2021-09-22 00:40:56 +0300

    path fixed!

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App(){
 
     return(
         <Router>
-            <Route path="socket-webchat/" exact component={Join} />
-            <Route path="socket-webchat/chat"  component={Chat} />
+            <Route path="/socket-webchat/"  component={Join} />
+            <Route path="/socket-webchat/chat"  component={Chat} />
         </Router>
     );
 }
